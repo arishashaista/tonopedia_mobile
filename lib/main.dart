b796620ed9b3002b4cd02f0a5ee2e75251651f9e
arishashaista <arishashaistaa@gmail.com>
2024-11-04 19:46:38 +0700

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tonopedia_mobile/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.lightGreen,
+          ).copyWith(secondary: Colors.lightGreen[900]),
+          useMaterial3: true,
+        ),
+        home: MyHomePage());
+  }
+}
